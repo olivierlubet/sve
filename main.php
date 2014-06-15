0e2155656bb93858ca35bd5250d5379f3c90f4b2
olivierlubet <olivier@lubet.net>
2014-06-15 22:15:24 +0200

    push

---
+++ main.php
@@ -0,0 +1,44 @@
+<?php
+include_once("include.php");
+
+if ($argc < 3 ) {
+?>
+
+C'est une ligne de commande à une option.
+
+  Utilisation :
+  <?php echo $argv[0]; ?> population period
+
+  population Integer : 3
+  period Integer : 90
+
+<?php
+} else {
+    
+    echo "population:".intval($argv[1])."\n";
+    echo "period:".intval($argv[2])."\n";
+    
+    $w = new sve\World('ACA.PA',intval($argv[1]),intval($argv[2]));
+    
+    $century=0;
+    $nbToSave=round($argv[1]*0.1)+1;
+    while(true)
+    {
+        echo "computing\n";
+        $w->compute(1);
+        $century++;
+        
+        echo "century:$century\n";
+        
+        
+        echo "saving $nbToSave bests\n";
+        $pop=$w->getPopulation();
+        for($i=0;$i<$nbToSave;$i++)
+        {
+            $agent= $pop[$i];
+            $agent->save();
+            echo $agent->getName().":\t".$agent->getLast()->getValue()."\n";
+        }
+    }
+}
+?>
